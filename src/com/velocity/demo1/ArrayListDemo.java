8a542d63002c3e63c3e9ee161d2d99bc04b85f3e
JayashreeWalse <jayashree.walse3@gmail.com>
2022-12-26 17:29:40 +0530

    first commit : added arraylist demo code

---
+++ src/com/velocity/demo1/ArrayListDemo.java
@@ -0,0 +1,17 @@
+package com.velocity.demo1;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+		ArrayList<String> list=new ArrayList<String>();
+		
+		list.add("Viraj");
+		list.add("Jayashree");
+		list.add("Rupesh");
+		System.out.println(list);
+		
+	}
+
+}
